cf5a0a7876e7caa5faa8f419a847056177a63145
Gryxon0x <fabiofabio101@o2.pl>
2026-04-01 22:22:12 +0200

    added payloads for main loop

--- src/main.c
+++ src/main.c
@@ -232,6 +232,11 @@ int main(void)
 	advertising_start();
 
 	for (;;) {
+		uint8_t payload[4];
 
+		payload[0] = (counter >> 0) & 0xFF;
+		payload[1] = (counter >> 8) & 0xFF;
+		payload[2] = (counter >> 16) & 0xFF;
+		payload[3] = (counter >> 24) & 0xFF;
 	}
 }
