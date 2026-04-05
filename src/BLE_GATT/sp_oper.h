fc54638bed0a28caca8922bcbfb964eac6de8952
Gryxon0x <fabiofabio101@o2.pl>
2026-04-05 23:59:26 +0200

    added sp_oper.h logic

---
+++ src/BLE_GATT/sp_oper.h
@@ -0,0 +1,19 @@
+#ifndef SP_PROV_H_
+#define SP_PROV_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <zephyr/bluetooth/conn.h>
+
+typedef void (*sp_prov_rx_cb_t)(const uint8_t *data, uint16_t len);
+
+int sp_prov_init(sp_prov_rx_cb_t rx_cb);
+
+void sp_prov_connected(struct bt_conn *conn);
+void sp_prov_disconnected(struct bt_conn *conn);
+
+bool sp_prov_is_tx_enabled(void);
+int sp_prov_send(const uint8_t *data, uint16_t len);
+int sp_prov_send_state(uint8_t state, uint8_t flags, uint16_t last_error);
+
+#endif /* SP_PROV_H_ */
