131fb2146d5b888786d25f5ea0d1c1c58a3813ba
Gryxon0x <fabiofabio101@o2.pl>
2026-04-17 23:18:53 +0200

    added command definitions in sp_oper

--- src/BLE_GATT/sp_oper.h
+++ src/BLE_GATT/sp_oper.h
@@ -8,6 +8,19 @@
 typedef void (*sp_oper_auth_rx_cb_t)(const uint8_t *data, uint16_t len);
 typedef void (*sp_oper_cmd_rx_cb_t)(const uint8_t *data, uint16_t len);
 
+enum {
+	SP_OPER_CMD_AUTH_GET_CHALLENGE = 0x20,
+	SP_OPER_CMD_AUTH_SEND_PROOF    = 0x21,
+	SP_OPER_CMD_AUTH_LOGOUT        = 0x22,
+};
+
+enum {
+	SP_OPER_EVT_AUTH_CHALLENGE     = 0x90,
+	SP_OPER_EVT_AUTH_ACK           = 0x91,
+	SP_OPER_EVT_AUTH_ERROR         = 0x92,
+	SP_OPER_EVT_AUTH_RESULT        = 0x93,
+};
+
 int sp_prov_init(sp_prov_rx_cb_t rx_cb);
 
 void sp_prov_connected(struct bt_conn *conn);
